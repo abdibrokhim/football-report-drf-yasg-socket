15dce516ff94e792fc1fd3257aa6cba6283b47d4
abdibrokhim <abdibrokhim@gmail.com>
2022-09-13 19:10:52 +0500

    smth

--- app/client.py
+++ app/client.py
@@ -1,6 +1,19 @@
 from channels.consumer import AsyncConsumer
-from random import randint
-from time import sleep
+from app import models, serializers
+from asgiref.sync import sync_to_async
+
+
+@sync_to_async
+def _get_data():
+    games = models.Game.objects.all()
+    serializer = serializers.GameSerializer(games, many=True)
+    return serializer.data
+
+
+@sync_to_async
+def _get_datas():
+    game = models.Game.objects.all()
+    return game
 
 
 class PracticeConsumer(AsyncConsumer):
@@ -8,14 +21,24 @@ class PracticeConsumer(AsyncConsumer):
         print("connected", event)
 
         await self.send({"type": "websocket.accept"})
-        await self.send({"type": "websocket.send", "text": 0})
 
-    async def websocket_receive(self, event):
-        print("receive", event)
+        game = await _get_datas()
+        print(game)
 
-        sleep(1)
+        data = await _get_data()
+        print(data)
 
-        await self.send({"type": "websocket.send", "text": str(randint(0, 100))})
+        await self.send({"type": "websocket.send", "text": data})
+
+    # async def websocket_receive(self, event):
+    #     print("receive", event)
+    #
+    #     data = await _get_data()
+    #     print(data)
+    #
+    #     sleep(1)
+    #
+    #     await self.send({"type": "websocket.send", "text": data})
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
